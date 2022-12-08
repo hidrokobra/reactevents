551f19511893270726830883f936a7ff79d00b93
DESKTOP-FAHGOIE\Hidrokobra <orlandou.hk@gmail.com>
2022-12-08 15:42:59 -0500

    se modifica index APP.js y se añaden CSS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './formularios.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div class="bg">
+        <div class="login">
+        <img src="https://i.imgur.com/F1YozVZ.png" class="pera" />
+          <div class="formulario">
+            <form>
+                <label for="fname"  id="email">E-mail</label><br />
+                <input type="text" id="femail" name="usuario" placeholder="example@mail.com" />       
+                <br />
+                <label for="fname" id="password">Password</label><br />
+                <input type="password" id="fpassword" name="contrasena" placeholder="**********" />
+                <label for="fname"  id="languaje">Language</label>
+                <select id="idioma" name="idioma" onChange="Idioma()">
+                    <option value="en">English</option>
+                    <option value="es">Español</option>
+                    <option value="fr">Français</option>
+                </select>
+            </form>
+            <br />
+            <label class="manzana" id="rememberme">Remember me
+                <input type="checkbox" />
+                <span class="checkbonito"></span>
+            </label>
+            <br />
+            <input type="submit" value="Login" onclick="Login()" />
+            <br />
+            <h2 class="linea"><span id="other">Login with</span></h2>
+            <br />
+            <center>
+                <img src="https://i.imgur.com/ZyhMKBO.png" />
+                <img src="https://i.imgur.com/Gh3eTe8.png" />
+                <img src="https://i.imgur.com/TsRroTk.png" />
+            </center>
+          </div>
+          </div></div>
       </header>
     </div>
   );
