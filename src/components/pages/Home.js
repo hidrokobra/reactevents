71879708aea34f70163efe13ac19ada6e70cbf6c
DESKTOP-7HGEHL2\Zentoku <orlandou.hk@gmail.com>
2022-12-21 19:07:59 -0500

    Desing a home page for the project before the login

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import '../CSS/formularios.css';
+import '../CSS/index.css';
 
 export default function Home() {
-  return <h1>Prueba</h1>;
-}
+  return (
+    <>
+        <div>
+          <br /> <br />
+          <table>
+            <tr>
+              <td colSpan={2}>
+                <img src="https://i.imgur.com/F1YozVZ.png" class="pera" />
+                <h1 class="title1">A way to organice your schedule!</h1>
+              </td>
+            </tr>
+            <br />
+            <tr>
+              <td>
+                <img src="https://i.imgur.com/F1YozVZ.png" class="pera2" />
+                <h1 class="title2">A way to organice your schedule!</h1>
+              </td>
+              <td>
+                <img src="https://i.imgur.com/F1YozVZ.png" class="pera2" />
+                <h1 class="title2">A way to organice your schedule!</h1>
+              </td>
+            </tr>
+          </table>
+        </div>
+    </>
+  );
+};
