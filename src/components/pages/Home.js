e7c0fed996ce9bf0e5b98d7a4bf07a7a1568e0bd
DESKTOP-7HGEHL2\Zentoku <orlandou.hk@gmail.com>
2022-12-25 08:15:13 -0500

    Editing home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,18 +11,18 @@ export default function Home() {
             <tr>
               <td colSpan={2}>
                 <img src="https://i.imgur.com/F1YozVZ.png" class="pera" />
-                <h1 class="title1">A way to organice your schedule!</h1>
+                <h1 class="title1">Don't get lost on your shedule events!</h1>
               </td>
             </tr>
             <br />
             <tr>
               <td>
                 <img src="https://i.imgur.com/F1YozVZ.png" class="pera2" />
-                <h1 class="title2">A way to organice your schedule!</h1>
+                <h1 class="title2">Organice your time properly</h1>
               </td>
               <td>
                 <img src="https://i.imgur.com/F1YozVZ.png" class="pera2" />
-                <h1 class="title2">A way to organice your schedule!</h1>
+                <h1 class="title2">Get notifications via email or google calendar!</h1>
               </td>
             </tr>
           </table>
