175c5eddf7062f087972c79563087581616fac25
DESKTOP-FAHGOIE\Hidrokobra <orlandou.hk@gmail.com>
2023-01-11 23:14:57 -0500

    Create forms for the database

--- src/components/pages/CreateEvent.js
+++ src/components/pages/CreateEvent.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import '../CSS/formularios.css';
 
 export default function Createevent() {
   return (
     <>
-      <h1 className='consulting'>Create event</h1>
+      <div class="newevent">
+        <div class="formulario">
+          <form>
+            <label for="fname"  id="email">E-mail</label><br />
+            <input type="text" id="femail" name="usuario" placeholder="example@mail.com" />       
+            <br />
+            <label for="fname" id="password">Password</label><br />
+            <input type="password" id="fpassword" name="contrasena" placeholder="**********" />
+            <label for="fname"  id="languaje">Language</label>
+            <select id="idioma" name="idioma" onChange="Idioma()">
+                <option value="en">English</option>
+                <option value="es">Español</option>
+                <option value="fr">Français</option>
+            </select>
+          </form>
+          <br />
+        </div>
+      </div>
     </>
   );
 }
